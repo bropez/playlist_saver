13e347b22089e30ddeb2a000b0e2ed2a8adae3c6
matthew <matthewbropez@gmail.com>
2019-02-15 17:31:35 -0500

    edited the frontend and backend so they could work together

--- save_playlist.py
+++ save_playlist.py
@@ -5,54 +5,56 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy.util as util
 
-# grab all credentials from a separate file
-username = cfg.spotify['username']
-client_id = cfg.spotify['client_id']
-client_secret = cfg.spotify['client_secret']
-redirect_uri = cfg.spotify['redirect_uri']
 
-scope = 'user-library-read playlist-modify-public playlist-read-private'
+def create_playlist(source_name, new_name):
+    # grab all credentials from a separate file
+    username = cfg.spotify['username']
+    client_id = cfg.spotify['client_id']
+    client_secret = cfg.spotify['client_secret']
+    redirect_uri = cfg.spotify['redirect_uri']
 
-# use credentials for spotify
-client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+    scope = 'user-library-read playlist-modify-public playlist-read-private'
 
-# creating the spotify object with credentials
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+    # use credentials for spotify
+    client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
 
-# creating a token, if possible, with user credentials
-token = util.prompt_for_user_token(username, scope, client_id, client_secret, redirect_uri)
+    # creating the spotify object with credentials
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-# login successful or unsuccessful
-if token:
-    sp = spotipy.Spotify(auth=token)
-    print("Login successful")
-else:
-    print("Can't get token for ", username)
+    # creating a token, if possible, with user credentials
+    token = util.prompt_for_user_token(username, scope, client_id, client_secret, redirect_uri)
 
-# get all songs from one playlist and store in variable
-# spotify:user:spotify:playlist:ENTER_SOURCE_PLAYLIST_ID
-# my "tastebreakers" to get songs from
-source_playlist_id = input("Copy and paste source playlist's ID: ")
-source_playlist = sp.user_playlist(username, source_playlist_id)
-print("Using '" + source_playlist['name'] + "' as source playlist.")
-tracks = source_playlist['tracks']
-songs = tracks["items"]
+    # login successful or unsuccessful
+    if token:
+        sp = spotipy.Spotify(auth=token)
+        print("Login successful")
+    else:
+        print("Can't get token for ", username)
 
-# filling a list of track id's
-# and only track id's, apparently that's how "user_playlist_add_tracks()"
-# takes in tracks
-track_ids = []
-for i in range(0, len(songs)):
-    track_ids.append(songs[i]['track']['id'])
-    # print(track_ids[i])
+    # get all songs from one playlist and store in variable
+    # spotify:user:spotify:playlist:ENTER_SOURCE_PLAYLIST_ID
+    # my "tastebreakers" to get songs from
+    source_playlist_id = source_name
+    source_playlist = sp.user_playlist(username, source_playlist_id)
+    print("Using '" + source_playlist['name'] + "' as source playlist.")
+    tracks = source_playlist['tracks']
+    songs = tracks["items"]
 
-# put all songs into playlist I've created
-created_name = input("Please enter your new playlist's name: ")
-my_playlist = sp.user_playlist_create(username, name=created_name)
-print("Playlist " + created_name + " has been created.")
-sp.user_playlist_add_tracks(
-    username,
-    playlist_id=my_playlist['id'],
-    tracks=track_ids
-)
-print("We've added all the songs from the source playlist to your selected playlist")
+    # filling a list of track id's
+    # and only track id's, apparently that's how "user_playlist_add_tracks()"
+    # takes in tracks
+    track_ids = []
+    for i in range(0, len(songs)):
+        track_ids.append(songs[i]['track']['id'])
+        # print(track_ids[i])
+
+    # put all songs into playlist I've created
+    created_name = new_name
+    my_playlist = sp.user_playlist_create(username, name=created_name)
+    print("Playlist " + created_name + " has been created.")
+    sp.user_playlist_add_tracks(
+        username,
+        playlist_id=my_playlist['id'],
+        tracks=track_ids
+    )
+    print("We've added all the songs from the source playlist to your selected playlist")
--- spotify_sketch.py
+++ spotify_sketch.py
@@ -1,5 +1,9 @@
+import tkinter as tk
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
+
+import save_playlist
 
 
 def reset():
@@ -9,7 +13,21 @@ def reset():
 
 
 def create(*args):
-    print("Create has been pressed")
+    source_name = source.get()
+    playlist_name = new_playlist.get()
+    message_box = tk.messagebox.askquestion(
+        "Create the playlist",
+        "Are you sure you want to create " + playlist_name + "?"
+    )
+    if message_box == "yes":
+        save_playlist.create_playlist(source_name, playlist_name)
+        print("We created the " + playlist_name + " playlist")
+    else:
+        tk.messagebox.showinfo("Return", "It's okay, we can all try again")
+
+
+def close_app():
+    root.destroy()
 
 
 root = Tk()
@@ -26,13 +44,13 @@ new_playlist = StringVar()
 source_entry = ttk.Entry(mainframe, width=30, textvariable=source)
 source_entry.grid(column=2, row=0, sticky=(W, E))
 new_pl_entry = ttk.Entry(mainframe, width=30, textvariable=new_playlist)
-new_pl_entry.grid(column=2, row=1, sticky=(W, E))
+new_pl_entry.grid(column=2, row=1, sticky=E)
 
 ttk.Label(mainframe, text="Source Playlist").grid(column=0, row=0, sticky=W)
 ttk.Label(mainframe, text="New Playlist Name").grid(column=0, row=1, sticky=W)
 
 ttk.Button(mainframe, text="Reset", command=reset).grid(column=0, row=2, sticky=E)
-ttk.Button(mainframe, text="Create", command=create).grid(column=2, row=2, sticky=W)
+ttk.Button(mainframe, text="Create", command=create).grid(column=2, row=2, sticky=E)
 
 for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
 
